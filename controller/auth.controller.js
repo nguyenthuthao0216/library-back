7f11bfac74a72963c1016e696e9feed479626579
nguyenthuthao0216 <nguyenthuthao0216@gmail.com>
2023-10-19 23:57:08 +0700

    first

---
+++ controller/auth.controller.js
@@ -0,0 +1,37 @@
+const User = require("../model/User");
+const mongoose = require("mongoose");
+
+const AuthController = {
+  login: async (req, res) => {
+    try {
+      const { username, password } = req.body;
+
+      const checkUser = await User.findOne({ username: username });
+
+      // checkUser dang co ton tai
+
+      if (!checkUser) {
+        // check User khong co
+        return res.status(404).json({
+          message: "Người dùng không tồn tại",
+        });
+      }
+      if (password != checkUser.password) {
+        return res.status(400).json({
+          message: "Wrong password",
+        });
+      }
+      return res.status(200).json({
+        message: "Đăng nhập thành công",
+        user: checkUser,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        message: "Server error",
+        error: error,
+      });
+    }
+  },
+};
+
+module.exports = AuthController;
